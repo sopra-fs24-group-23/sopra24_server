b8bd35d56415991055d136fee9cd8628ac690318
Jonas Krumm <jonas@krumm.ch>
2024-03-26 14:39:57 +0100

    renamed to UserRepository, as Players are no longer persisted

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package ch.uzh.ifi.hase.soprafs24.repository;
+
+import ch.uzh.ifi.hase.soprafs24.entity.Player;
+import ch.uzh.ifi.hase.soprafs24.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository("userRepository")
+public interface UserRepository extends JpaRepository<User, Long> {
+  User findByUsername(String username);
+  User findByToken(String token);
+}
+
