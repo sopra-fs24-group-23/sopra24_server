4971a459303499e86fa28395ab3c549641ba9dcc
Dedphish <jonas@krumm.ch>
2024-03-29 14:15:34 +0100

    added createLobby PostMapping

--- src/main/java/ch/uzh/ifi/hase/soprafs24/controller/LobbyController.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/controller/LobbyController.java
@@ -1,7 +1,31 @@
 package ch.uzh.ifi.hase.soprafs24.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import ch.uzh.ifi.hase.soprafs24.entity.Lobby;
+import ch.uzh.ifi.hase.soprafs24.entity.User;
+import ch.uzh.ifi.hase.soprafs24.rest.dto.LobbyStateDTO;
+import ch.uzh.ifi.hase.soprafs24.rest.dto.UserTokenDTO;
+import ch.uzh.ifi.hase.soprafs24.rest.mapper.DTOMapper;
+import ch.uzh.ifi.hase.soprafs24.service.LobbyService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+
 @RestController
 public class LobbyController {
+
+    private final LobbyService lobbyService;
+
+    public LobbyController(LobbyService lobbyService) {
+        this.lobbyService = lobbyService;
+    }
+
+    @PostMapping("/lobbies")
+    @ResponseStatus(HttpStatus.CREATED)
+    @ResponseBody
+    public LobbyStateDTO createLobby(@RequestBody UserTokenDTO userTokenDTO) {
+        User host = DTOMapper.INSTANCE.convertUserTokenDTOtoEntity(userTokenDTO);
+        Lobby createdLobby = lobbyService.createLobby(host);
+        return DTOMapper.INSTANCE.convertEntityToLobbyStateDTO(createdLobby);
+    }
 
 }
