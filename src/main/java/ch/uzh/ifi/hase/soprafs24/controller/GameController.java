f368db2c1a7c9fbc03d0080cb7353bb47fef4d30
fr4n715k4 <108323504+fr4n715k4@users.noreply.github.com>
2024-04-08 21:53:33 +0200

    Start setting up GameController

--- src/main/java/ch/uzh/ifi/hase/soprafs24/controller/GameController.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/controller/GameController.java
@@ -1,4 +1,39 @@
 package ch.uzh.ifi.hase.soprafs24.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import ch.uzh.ifi.hase.soprafs24.rest.dto.GameDTO;
+
+import ch.uzh.ifi.hase.soprafs24.rest.dto.LobbyIdDTO;
+import ch.uzh.ifi.hase.soprafs24.rest.mapper.DTOMapper;
+import ch.uzh.ifi.hase.soprafs24.service.LobbyService;
+import ch.uzh.ifi.hase.soprafs24.service.GameService;
+import ch.uzh.ifi.hase.soprafs24.entity.Lobby;
+import ch.uzh.ifi.hase.soprafs24.entity.Game;
+
+@RestController
 public class GameController {
+
+    private final LobbyService lobbyService;
+    private final GameService gameService;
+
+    public GameController(LobbyService lobbyService, GameService gameService) {
+        this.lobbyService = lobbyService;
+        this.gameService = gameService;
+    }
+
+
+   @PostMapping("/games")
+   @ResponseStatus(HttpStatus.CREATED)
+   @ResponseBody
+   public GameDTO createGame(@RequestBody LobbyIdDTO lobbyIdDTO) {
+    Lobby lobby = lobbyService.getLobbyById(lobbyIdDTO.getId());
+    Game createdGame = gameService.createGame(lobby);
+
+    return DTOMapper.INSTANCE.convertEntityToGameDTO(createdGame);
+   }
 }
