70b703141366a2a94565bd7590361ca673be1dcd
fr4n715k4 <108323504+fr4n715k4@users.noreply.github.com>
2024-04-08 21:52:58 +0200

    Randomize letter when round starts

--- src/main/java/ch/uzh/ifi/hase/soprafs24/entity/Round.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/entity/Round.java
@@ -1,18 +1,21 @@
 package ch.uzh.ifi.hase.soprafs24.entity;
 
 import java.util.List;
+import java.util.Random;
 
 public class Round {
     private Integer roundNumber;
-    private String letter;
+    private String[] letters;
     private Integer timer;
     private List<String> answers;
+    private String startLetter;
 
     private Game mainGame;
 
-    public Round(Integer roundNumber, String letter, Game mainGame) {
+    public Round(Integer roundNumber, Game mainGame) {
         this.roundNumber = roundNumber;
-        this.letter = letter;
+        this.letters = new String[] {"A", "B", "C", "D"};
+        this.startLetter = generateRandomLetter();
         this.mainGame = mainGame;
     }
 
@@ -22,6 +25,14 @@ public class Round {
     public void endRound() {
 
     }
+
+    public String generateRandomLetter() {
+        Random rand = new Random();
+        int randomIndex = rand.nextInt(26);
+
+        return letters[randomIndex];
+    }
+
     public void displayAnswers() {}
     public void checkAnswers() {}
     public void calculateScores() {}
