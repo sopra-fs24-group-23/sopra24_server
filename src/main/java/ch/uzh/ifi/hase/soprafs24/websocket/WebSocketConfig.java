5cf79b884f146417bd6b3c2bdc676f411a88efb5
Dedphish <jonas@krumm.ch>
2024-04-09 12:41:32 +0200

    configured websocket heartbeats

--- src/main/java/ch/uzh/ifi/hase/soprafs24/websocket/WebSocketConfig.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/websocket/WebSocketConfig.java
@@ -1,6 +1,9 @@
 package ch.uzh.ifi.hase.soprafs24.websocket;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.TaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -8,6 +11,13 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    private TaskScheduler messageBrokerTaskScheduler;
+
+    @Autowired
+    public void setMessageBrokerTaskScheduler(@Lazy TaskScheduler taskScheduler) {
+        this.messageBrokerTaskScheduler = taskScheduler;
+    }
 
     @Override
     public void registerStompEndpoints (StompEndpointRegistry registry) {
@@ -31,7 +41,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/app");
-        registry.enableSimpleBroker("/topic");
+        registry.enableSimpleBroker("/topic")
+                .setHeartbeatValue(new long[] {10000, 10000})
+                .setTaskScheduler(this.messageBrokerTaskScheduler);
+        ;
     }
 }
 
