2ccd9d6860d38ceeb27faa954e6e79cbf1782c40
Dedphish <jonas@krumm.ch>
2024-04-23 16:25:38 +0200

    vote entity

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/entity/Vote.java
@@ -0,0 +1,22 @@
+package ch.uzh.ifi.hase.soprafs24.entity;
+
+public class Vote {
+    private String username;
+    private String category;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
