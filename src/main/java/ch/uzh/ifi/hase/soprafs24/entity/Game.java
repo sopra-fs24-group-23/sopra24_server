e5d4c3cb7b73f27de0192067dd834110d49a5c4b
fr4n715k4 <108323504+fr4n715k4@users.noreply.github.com>
2024-04-26 21:53:16 +0200

    Throw exception in doubtAnswers

--- src/main/java/ch/uzh/ifi/hase/soprafs24/entity/Game.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/entity/Game.java
@@ -226,6 +226,7 @@ public class Game {
 
     // this is pretty disgusting... wopsieee
     public void doubtAnswers(String username, List<Vote> votes) throws PlayerNotFoundException {
+        boolean playerExists = false;
         for (Vote vote : votes) {
             for (Player p : players) {
                 if (p.getUsername().equals(vote.getUsername())) {
@@ -234,8 +235,12 @@ public class Game {
                             a.setIsDoubted(true);
                         }
                     }
+                    playerExists = true;
                 }
             }
+        }
+        if (!playerExists) {
+            throw new PlayerNotFoundException("Player not found");
         }
         for (Player p : players) {
             if (p.getUsername().equals(username)) {
