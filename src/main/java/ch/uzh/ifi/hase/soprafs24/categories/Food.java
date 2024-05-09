6e4a44e256fc15477c399f2301c09ca8e5fab509
nilaksan97 <nilaksan.selliah@uzh.ch>
2024-05-09 13:19:46 +0200

    food api fix

--- src/main/java/ch/uzh/ifi/hase/soprafs24/categories/Food.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/categories/Food.java
@@ -4,7 +4,7 @@ import ch.uzh.ifi.hase.soprafs24.api.FoodAPI;
 
 public class Food implements Category {
     private static final String API_KEY = "7f9f1b12c5msh0ee2d0b9a2cbbb7p158dc9jsn62d752680b9e";
-    private static final String BASE_URL = "https://nutrition-by-api-ninjas.p.rapidapi.com/v1/";
+    private static final String BASE_URL = "https://nutrition-by-api-ninjas.p.rapidapi.com";
 
     @Override
     public boolean validateAnswer(String answer) {
