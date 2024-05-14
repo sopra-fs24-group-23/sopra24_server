1cd2e847a870eb2c66e944ded0b2a3d9d16e6fba
nilaksan97 <nilaksan.selliah@uzh.ch>
2024-05-14 19:31:27 +0200

    Added WebConfig file

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/config/WebConfig.java
@@ -0,0 +1,22 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedOrigin("https://sopra-fs24-group-23-client.oa.r.appspot.com");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
--- src/main/java/ch/uzh/ifi/hase/soprafs24/config/WebSocketConfig.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/config/WebSocketConfig.java
@@ -20,24 +20,12 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     }
 
     @Override
-    public void registerStompEndpoints (StompEndpointRegistry registry) {
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-        .setAllowedOrigins(
-                "http://localhost:3000/",
-                "https://sopra-fs24-group-23-client.oa.r.appspot.com/"
-        )
+                .setAllowedOrigins("http://localhost:3000", "https://sopra-fs24-group-23-client.oa.r.appspot.com")
         .setAllowedOriginPatterns("*");
     }
 
-    /** This message configures websocket endpoint
-     * - It sets /app as the PREFIX for endpoints that are handled directly by the application
-     * i.e. for @MessageMappings in controller classes. (Client -> Server messages)
-     * - It sets /topic as the PREFIX for endpoints that clients can subscribe to
-     * i.e. for template.convertAndSend("/topic/sometopic", Message) or @SendTo
-     * Server -> Client messaging, one-to-many broadcast
-     * - It can also register a prefix for specific user destinations by using
-     * setUserDestinationPrefixes; Server -> Client messaging, point-to-point
-     * **/
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/app");
@@ -46,4 +34,3 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
                 .setTaskScheduler(this.messageBrokerTaskScheduler);
     }
 }
-
