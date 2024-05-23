5282947cbb363a471f62f96f39ac2842541f5cd1
Dedphish <jonas@krumm.ch>
2024-05-23 20:37:28 +0200

    add test for handleGameStateChange

--- src/test/java/ch/uzh/ifi/hase/soprafs24/websocket/GameWebsocketControllerTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs24/websocket/GameWebsocketControllerTest.java
@@ -5,23 +5,20 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import ch.uzh.ifi.hase.soprafs24.entity.*;
+import ch.uzh.ifi.hase.soprafs24.rest.dto.PlayerGetDTO;
+import org.mockito.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 
 import ch.uzh.ifi.hase.soprafs24.constant.GamePhase;
 import ch.uzh.ifi.hase.soprafs24.controller.GameWebsocketController;
-import ch.uzh.ifi.hase.soprafs24.entity.Answer;
-import ch.uzh.ifi.hase.soprafs24.entity.GameState;
-import ch.uzh.ifi.hase.soprafs24.entity.Player;
 import ch.uzh.ifi.hase.soprafs24.events.GameStateChangeEvent;
 import ch.uzh.ifi.hase.soprafs24.rest.dto.GameStateDTO;
 import ch.uzh.ifi.hase.soprafs24.service.GameService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 
 import java.util.ArrayList;
@@ -46,7 +43,7 @@ public class GameWebsocketControllerTest {
     }
 
     @Test
-    public void testHandleGameStateChange() {
+    public void handleGameStateChange_validInputs_sendsMessageToClients() {
         // Given
         Player player1 = new Player(1L,"player1","qsdwq", "#000000");
         Player player2 =  new Player(2L,"player2","fre", "#000000");
@@ -68,7 +65,7 @@ public class GameWebsocketControllerTest {
     }
 
     @Test
-    public void testReceiveAnswers() {
+    public void receiveAnswers_validInputs_setsAnswersThroughGameService() {
         // Given
         String lobbyId = "lobbyId";
         String username = "username";
@@ -83,10 +80,10 @@ public class GameWebsocketControllerTest {
     }
 
     @Test
-    public void testUpdateClients() {
+    public void updateClients_validInputs_informsClients() {
         // Given
         Player player1 = new Player(1L,"player1","qsdwq", "#000000");
-       Player player2 =  new Player(2L,"player2","fre", "#000000");
+        Player player2 =  new Player(2L,"player2","fre", "#000000");
 
         List<Player> players = new ArrayList<>();
         players.add(player1);
@@ -106,7 +103,7 @@ public class GameWebsocketControllerTest {
     }
 
     @Test
-    public void testCloseInputs() {
+    public void closeInput_validInputs_callsGameService() {
         // Given
         String lobbyId = "lobbyId";
 
@@ -117,6 +114,58 @@ public class GameWebsocketControllerTest {
         verify(gameService).closeInputs(lobbyId);
     }
 
-    //Todo Continue adding tests for other methods such as 'receivedDoubt' and more.
+    @Test
+    public void handleGameStateChange_validInputs_sendsRelevantData() {
+
+        // initialize players & answers
+        Player player1 = new Player(1L,"player1","qsdwq", "#000000");
+        Answer answer1 = new Answer("City", "Paris");
+        List<Answer> answerList1 = new ArrayList<>();
+        answerList1.add(answer1);
+        player1.setCurrentAnswers(answerList1);
+        player1.setCurrentScore(100);
+
+        Player player2 =  new Player(2L,"player2","fre", "#000000");
+        Answer answer2 = new Answer("City", "Pittsburgh");
+        List<Answer> answerList2 = new ArrayList<>();
+        answerList2.add(answer2);
+        player2.setCurrentAnswers(answerList2);
+        player2.setCurrentScore(300);
+
+        // initialize game
+        List<Player> players = new ArrayList<>();
+        players.add(player1);
+        players.add(player2);
+
+        GameSettings settings = new GameSettings();
+
+        Game newGame = new Game(settings, players);
+        newGame.setCurrentLetter("P");
+
+        GameStateChangeEvent changeEvent = new GameStateChangeEvent(newGame, newGame.getState(), "testId");
+
+        ArgumentCaptor<GameStateDTO> captor = ArgumentCaptor.forClass(GameStateDTO.class);
+
+        controller.handleGameStateChange(changeEvent);
+
+        verify(messagingTemplate).convertAndSend(
+                eq("/topic/games/testId/state"),
+                captor.capture()
+        );
+
+        GameStateDTO sentDTO = captor.getValue();
+
+        assertEquals("P", sentDTO.getCurrentLetter());
+        assertEquals("Paris", sentDTO.getPlayers().get(0).getCurrentAnswers().get(0).getAnswer());
+        assertEquals("Pittsburgh", sentDTO.getPlayers().get(1).getCurrentAnswers().get(0).getAnswer());
+        for (PlayerGetDTO dto : sentDTO.getPlayers()) {
+            assertTrue(dto.getUsername().equals("player1") || dto.getUsername().equals("player2"));
+            assertTrue(
+                    dto.getCurrentAnswers().get(0).getAnswer().equals("Paris") ||
+                            dto.getCurrentAnswers().get(0).getAnswer().equals("Pittsburgh")
+            );
+            assertTrue(dto.getCurrentScore() == 100 || dto.getCurrentScore() == 300);
+        }
+    }
 
 }
